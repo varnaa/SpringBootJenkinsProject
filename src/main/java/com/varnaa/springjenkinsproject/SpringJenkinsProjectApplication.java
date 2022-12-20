748375e370559bf70405beb0d4877048ba28eeb8
Swethavarnaa U <swethavarnaa@gmail.com>
2022-12-20 23:12:54 +0530

    Initial Commit

---
+++ src/main/java/com/varnaa/springjenkinsproject/SpringJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.varnaa.springjenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/varnaa/springjenkinsproject/SpringJenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varnaa.springjenkinsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
